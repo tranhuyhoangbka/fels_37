383117e2b331d12a17f56993aaf9517b2cdd21d1
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-02-09 13:20:26 +0700

    login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  	  flash[:success] = "your account is created!"
+  	  redirect_to root_url
+  	else
+  	  render 'new'
+  	end
+  end
+
+  def edit
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  end
+
+  def index
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
