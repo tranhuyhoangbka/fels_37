f9af425ea97f87a767b0cb098e6d4ef23123467c
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-02-09 16:12:20 +0700

    Addtion signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }	
+  before_save {self.email = email.downcase}	
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   has_many :learneds
   has_many :results
   has_many :activities
   validates :name, presence: true, length: {maximum: 60}
-  validates :email, presence: true, length: {maximum: 80}, format: { with: VALID_EMAIL_REGEX },
-             uniqueness: { case_sensitive: false }
+  validates :email, presence: true, length: {maximum: 80}, format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}             
   validates :password, presence: true, length: {maximum: 80, minimum: 6}
   validates :password_confirmation, presence: true, length: {maximum: 80, minimum: 6}
   has_secure_password
