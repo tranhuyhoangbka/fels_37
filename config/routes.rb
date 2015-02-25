0735f46318b6088900424f22fb249005400cdc99
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-02-25 15:37:10 +0700

    create categories page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
-Rails.application.routes.draw do  
+Rails.application.routes.draw do    
+  resources :categories
   get 'word_lists' => 'word_lists#index'      
   get 'sessions/new'
   get 'login' => 'sessions#new'
