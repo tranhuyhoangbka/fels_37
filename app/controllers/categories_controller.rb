8d4b8a52f9549f18cfce7345abd15695c2a2a66d
tranhuyhoangbka <hoangth92.nd@gmail.com>
2015-02-26 21:54:32 +0700

    create lession and generate the result

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,6 +4,19 @@ class CategoriesController < ApplicationController
   end
 
   def show
+    @category = Category.find params[:id]
+    @words = @category.words.order('RANDOM()').limit(20)
+        
+    if params[:result]
+      @question_answer_id_list = params[:result]
+      answer_ids = @question_answer_id_list.values
+      @answers = Answer.where(id: answer_ids)
+    end
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def new
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,6 @@
 module CategoriesHelper
+  def correct_answer?(word_id, answer_id)
+    answers = Word.find(word_id).answers
+    answers.correct.first.id == answer_id
+  end
 end
